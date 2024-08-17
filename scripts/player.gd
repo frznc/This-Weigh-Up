a25dd201f85a0974e29e5c3c70e47f58792f90dd
CalebBay <cbay@oswego.edu>
2024-08-16 20:23:36 -0400

    8/16 8:23PM

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
-var weight = 0 # 0 (no weight) to 150 (10 weights) each weight adds 15
+@onready var world = "res://scenes/world.tscn"
+@onready var weight = "res://scenes/weight.tscn"
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity
@@ -12,7 +13,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump
 	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = (JUMP_VELOCITY + weight)
+		velocity.y = (JUMP_VELOCITY + (Global.weights * 15))
 
 	# Get the input direction and handle the movement/deceleration
 	var direction := Input.get_axis("left", "right")
@@ -20,5 +21,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	# Drop weights
+	if Input.is_action_just_pressed("drop") and Global.weights > 0:
+		Global.weights -= 1
+		# Add a weight back to the world at this position. i have no idea how to do this
+	
 	move_and_slide()
--- scripts/weight.gd
+++ scripts/weight.gd
@@ -5,9 +5,11 @@ var player_inside = false
 @onready var player = preload("res://scenes/player.tscn")
 
 func _physics_process(delta: float) -> void:
+	# Player has picked up the weight, add it to them
 	if (Input.is_action_just_pressed("pickup")):
 		if player_inside:
-			print("picked up")
+			Global.weights += 1
+			queue_free()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
