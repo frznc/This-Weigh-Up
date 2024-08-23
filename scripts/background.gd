2039ddc019107a5abb5e5f58a7453f53018234b3
nickbug7 <randomnecroz@gmail.com>
2024-08-22 21:55:56 -0400

    improvements to  lvl 1,2,3,4

--- scripts/background.gd
+++ scripts/background.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	for x in get_children():
 		if !x.name == "Timer":
 			x.play("default")
-
+	rotation = randi_range(0,180)
 	
 func _physics_process(delta: float) -> void:
 	rotation = rotation + 0.0003
